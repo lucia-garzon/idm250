243a63ceb3fe21c9bec1d56b4f4206cb8296f76b
lucia-garzon <lucgarzon.uni@gmail.com>
2025-01-29 21:04:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index.php</title>
+    <title>Document</title>
 </head>
 <body>
-    This is the index.php file!!!
+    
 </body>
 </html>
