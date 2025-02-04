2c77530dfb7daba61ab70f88c4c9ba0e79d948fa
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-04 17:37:30 -0500

    create header and footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer>
+        <div class="footer-content">
+        <div class="footer-info">
+            <!-- <div class="footer-logo"> 
+                <img class="footer-img" src="images/hand-logo.svg" alt="Hand with heart above">
+            </div> -->
+            <h3 class="footer-header">Stay in the loop with all things Sarah!</h3>
+            <p class="footer-text">An occasional newsletter with project announcements, reminders on public appearances, sneak peeks and more!</p>
+        </div>
+        
+        <div>
+            <form class="email-form" action="google.com" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate="">
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required="">
+                <input type="submit" id="mc-embedded-subscribe" class="btn btn--solid btn--black" value="sign me up">
+            </form>
+        </div>
+
+        </div>
+    </footer>
+</body>
+</html>
