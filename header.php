a01b568e8d25d8b710abb05733b03e820fc24228
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-05 09:57:21 -0500

    update header.php

--- header.php
+++ header.php
@@ -4,10 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title><?php 
+    wp_title('|', true, 'right');
+    bloginfo('name');
+    ?></title>
     <link rel="stylesheet" href="styles.css">
     <link rel="shortcut icon" type="image/svg" href="images/Lilypad-logo.svg">
+    <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
 
-<?php get_template_part('components/main-header'); ?>
+    <?php get_template_part('components/main-header'); ?>
