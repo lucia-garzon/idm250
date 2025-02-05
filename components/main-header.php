bc5fa7ef5c2c8da25a46bb9b987964b84ae5a5ef
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-04 20:08:10 -0500

    make main-header in components

---
+++ components/main-header.php
@@ -0,0 +1,24 @@
+<header>
+        <!-- navigation -->
+        <a class="logo" href="index.html">
+            <img alt="logo" src="images/sws-logo.svg" class="logo">
+        </a>
+        <nav class="nav-wrapper">
+            <ul class="nav-menu">
+                <li>
+                    <a class="nav-link">About</a>
+                </li>
+                <li>
+                    <a class="nav-link">Books</a>
+                </li>
+                <li>
+                    <a class="nav-link">Art</a>
+                </li>
+                <li>
+                    <a class="nav-link">Contact</a>
+                </li>
+            </ul>
+            <button class="btn_menu burger-x-button"><img src="images/Burger-x.svg" alt="Burger x out" class="burger-x"></button>
+        </nav> 
+        <button class="btn_menu burger"><img src="images/Lilypad-burger.svg" class="burger" alt="Hamburger menu"></button>
+    </header>
