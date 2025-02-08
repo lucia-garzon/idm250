2b8c03a0eb3290249822e63a752d1fc959064d36
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-07 22:32:01 -0500

    📝 update menu

--- components/main-header.php
+++ components/main-header.php
@@ -1,8 +1,11 @@
 <header>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/sws-logo.svg" alt="logo">
+        
         <nav class="nav-wrapper">
         
             <ul class="nav-menu">
+                <li class="logo">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/sws-logo.svg" alt="logo">
+                </li>
                 <li>
                     <a class="nav-link" href="page.php">About</a>
                 </li>
@@ -18,5 +21,6 @@
             </ul>
             <button class="btn_menu burger-x-button"><img src="images/Burger-x.svg" alt="Burger x out" class="burger-x"></button>
         </nav> 
-        <button class="btn_menu burger"><img src="images/Lilypad-burger.svg" class="burger" alt="Hamburger menu"></button>
+        <button class="btn_menu burger"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/burger-250.svg" class="burger" alt="Hamburger menu"></button>
+        
     </header>
