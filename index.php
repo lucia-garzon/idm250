09619df54563d10a3b0b6ee1a5800f5ddfa0217f
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-08 01:08:24 -0500

    📝 update index.php to get content

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
 
+<!-- <h1>
+    <?php 
+        echo get_the_title();
+    ?>
+</h1> -->
+
+<?php if (has_post_thumbnail()) : ?>
+    <div class="featured-image">
+        <?php the_post_thumbnail(); ?>
+    </div>
+<?php endif; ?>
+
+<div class="main-content">
+    <?php 
+        echo get_the_content();
+    ?>
+</div>
 <?php get_footer(); ?>
