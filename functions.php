7e050250cb6d011ffdb78e4aa7fd48a2b08c5bda
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-07 22:45:50 -0500

    📝 add featured image

--- functions.php
+++ functions.php
@@ -29,5 +29,17 @@ function theme_styles_and_scripts() {
 
 }
 
-
 add_action('wp_enqueue_scripts', 'theme_styles_and_scripts');
+
+
+function theme_setup(){
+    //enable support for Post Thumbnails
+    add_theme_support('post-thumbnails');
+
+    register_nav_menus([
+        'primary-menu'=>'Primary Menu',
+        'footer-menu'=>'Footer Menu',
+    ]);
+}
+
+add_action('after_setup_theme','theme_setup');
