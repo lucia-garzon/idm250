a363c92c16c21bff6c1603c011c00662e66c4bb2
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-19 00:49:32 -0500

    📝 update menu to match paul's

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const setupMobileNav = () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    menuToggle.addEventListener("click", function() {
+        menu.classList.toggle("active");
+    });
+    }
+
+    setupMobileNav();
+});
