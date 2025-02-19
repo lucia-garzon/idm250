7b6cdba1af24ad7cf63e29b0ec30e4cd36d3e45a
lucia-garzon <lucgarzon.uni@gmail.com>
2025-02-18 19:51:03 -0500

    📝 add outfit, update 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-This is a 404 page. 
+<div class="error-wrapper">
+    <p class="outfit-display">Whoops!</p>
+    <p class="outfit-204">404</p>
+    <h1>Page not found</h1>
+    <a href="front-page.php" class="back-home">Back home</a>
+</div>
+
+
 
 <?php get_footer(); ?>
