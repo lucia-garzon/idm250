fdc0012c258c670b845cca75e6cce80d8f7947fb
lucia-garzon <lucgarzon.uni@gmail.com>
2025-03-20 13:25:31 -0400

    📝 add feat image to home

--- front-page.php
+++ front-page.php
@@ -2,7 +2,14 @@
 
 <main>
 <div class="wrapper">
-    
+    <div class="featured-image">
+    <?php 
+    $hero_image = get_theme_mod('hero_image', get_template_directory_uri() . '/assets/images/hero-img.jpg'); 
+    if (!empty($hero_image)) {
+        echo '<img src="' . esc_url($hero_image) . '" alt="Hero Image">';
+    }
+    ?>
+    </div>
     <?php get_template_part('components/latest-projects'); ?>
     <?php get_template_part('components/latest-posts'); ?>
 </div>
