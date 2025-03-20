fa0e2d70bd795c07ce7959b21ccd599d7a554bf0
lucia-garzon <lucgarzon.uni@gmail.com>
2025-03-20 12:08:02 -0400

    📝 save the logo!

--- components/main-header.php
+++ components/main-header.php
@@ -1,30 +1,3 @@
-<!-- <header>
-        
-        <nav class="nav-wrapper">
-        
-            <ul class="nav-menu">
-                <li class="logo">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/sws-logo.svg" alt="logo">
-                </li>
-                <li>
-                    <a class="nav-link" href="page.php">About</a>
-                </li>
-                <li>
-                    <a class="nav-link">Books</a>
-                </li>
-                <li>
-                    <a class="nav-link">Art</a>
-                </li>
-                <li>
-                    <a class="nav-link">Contact</a>
-                </li>
-            </ul>
-            <button class="btn_menu burger-x-button"><img src="images/Burger-x.svg" alt="Burger x out" class="burger-x"></button>
-        </nav> 
-        <button class="btn_menu burger"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/burger-250.svg" class="burger" alt="Hamburger menu"></button>
-        
-    </header> -->
-
 <header class="header">
     <div class="header__logo">
         <?php if (has_custom_logo()) : ?>
@@ -35,7 +8,7 @@
         </div>
         <?php else: ?>
         <a href="<?php echo esc_url(home_url('/')); ?>" class="site-title">
-        <?php bloginfo('name'); ?>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/sws-logo.svg" alt="logo">
         </a>
         <?php endif; ?>
     </div>
