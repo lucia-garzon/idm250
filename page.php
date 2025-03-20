cac8c7133d7bf1057181ccba03524360c21999d7
lucia-garzon <lucgarzon.uni@gmail.com>
2025-03-20 13:25:44 -0400

    add feat image and content to page

--- page.php
+++ page.php
@@ -4,13 +4,13 @@
     <?php echo get_the_title(); ?> 
     </h1>
 
-<?php if (has_post_thumbnail()) : ?>
-<div class="featured-image">
-    <?php the_post_thumbnail(); ?>
-</div>
-<?php endif; ?>
+    <?php if (has_post_thumbnail()) : ?>
+    <div class="featured-image">
+        <?php the_post_thumbnail(); ?>
+    </div>
+    <?php endif; ?>
 
-<?php echo get_the_content(); ?>
+    <?php echo get_the_content(); ?>
 </div>
 
 <?php get_footer(); ?>
