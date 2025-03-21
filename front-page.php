7a07df7b821802cd16c3ffb4c3773aa9ad25a99f
lucia-garzon <lucgarzon.uni@gmail.com>
2025-03-21 00:53:42 -0400

    📝 del latest-posts

--- front-page.php
+++ front-page.php
@@ -11,7 +11,6 @@
     ?>
     </div>
     <?php get_template_part('components/latest-projects'); ?>
-    <?php get_template_part('components/latest-posts'); ?>
 </div>
 </main>
 <?php get_footer();?>
